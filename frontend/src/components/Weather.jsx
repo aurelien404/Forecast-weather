bad0e8e7d972bb141808637666779ac5d9b3f049
aurelienj <contact@aurelienj.ch>
2025-08-18 15:00:17 +0200

    Feat: Adding Capital on fallbackcity

--- frontend/src/components/Weather.jsx
+++ frontend/src/components/Weather.jsx
@@ -119,11 +119,40 @@ const Weather = ({ i18n, city, coords }) => {
     return dirs[index];
   }
 
-  const fallbackCity = {
-    name: "Ullapool",
-    latitude: 57.8954,
-    longitude: 5.1613,
-    country_code: "GB",
+  const fallbackCity = () => {
+    const lng = i18n;
+    let defaultCity;
+
+    if (lng.includes("CH")) {
+      defaultCity = {
+        name: "Bern",
+        latitude: 46.9481,
+        longitude: 7.4474,
+        country_code: "CH",
+      };
+    } else if (lng.startsWith("fr")) {
+      defaultCity = {
+        name: "Paris",
+        latitude: 48.8566,
+        longitude: 2.3522,
+        country_code: "FR",
+      };
+    } else if (lng.includes("GB") || lng.startsWith("en")) {
+      defaultCity = {
+        name: "London",
+        latitude: 51.5074,
+        longitude: -0.1278,
+        country_code: "GB",
+      };
+    } else {
+      defaultCity = {
+        name: "Ullapool",
+        latitude: 57.8954,
+        longitude: 5.1613,
+        country_code: "GB",
+      };
+    }
+    return defaultCity;
   };
 
   const [activeCity, setActiveCity] = useState(city || fallbackCity);
